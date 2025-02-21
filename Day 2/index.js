8f0c43f421478ae898a2ea47c80c254269629c24
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-02-22 00:55:39 +0600

    Day 2 assignment done

---
+++ Day 2/index.js
@@ -0,0 +1,35 @@
+console.log("Day 2");
+var user = {
+    name: "Mohammad Mohiuddin",
+    age: 35,
+    isStudent: false,
+    favoriteProgmLang: ["JavaScript", "Python"]
+}
+console.log(user);
+
+let name = "Mohammad Mohiuddin";
+
+name = "sujon";
+
+console.log(name);
+
+const age = 35;
+console.log(age);
+// age = 40;
+// console.log(age);
+
+var user1 = {
+    name: "Rahim",
+    age: 35,
+    isStudent: false,
+    favoriteProgmLang: ["JavaScript", "Python"]
+}
+
+user1.name = "Karim";
+console.log(user1);
+
+const friends = ["Rahim", "Karim", "Jabbar"];
+console.log(friends);
+
+friends[1] = "Salam";
+console.log(friends);
